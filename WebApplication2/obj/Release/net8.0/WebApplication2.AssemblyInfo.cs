61c17586cdf25e025948a749a744d0643648f3c3
manish <neupanem01@gmail.com>
2025-01-31 10:38:21 +0545

    update

--- WebApplication2/obj/Release/net8.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Release/net8.0/WebApplication2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba18104a2abae1b32a0652310e9a798262997650")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13b32d1dbd83430e051513c6b57c1f5dd6d56111")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
